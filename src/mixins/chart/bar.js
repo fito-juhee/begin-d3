6227bebd3b7c221de3581f5db0db72798a75feae
Juhee Baek <juhee.baek@fitogether.com>
2020-04-27 15:51:01 +0900

    speed chart navigator create

--- src/mixins/chart/bar.js
+++ src/mixins/chart/bar.js
@@ -3,16 +3,20 @@ import * as d3 from "d3";
 const bar = {
   data: () => ({
     backGroundColor: "#343939",
-    width: 820,
-    height: 400,
-    margin: {top: 30, right: 30, bottom: 70, left: 60},
+    // width: 820,
+    // height: 400,
+    margin: {top: 30, right: 30, bottom: 100, left: 60},
     csvData: []
   }),
   computed: {
     width() {
-     
+     let width = 820 - this.margin.left - this.margin.right;
+     return width;
+    },
+    height() {
+     let height = 400 - this.margin.top - this.margin.bottom;
+     return height;
     }
-    
   },
   methods: {
     setCsvFile: async function(url) {
