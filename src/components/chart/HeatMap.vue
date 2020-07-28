615f139969197e24dac63582bcb08c035dee9295
Cheol_Kim <cheol.kim@fitogether.com>
2020-07-28 18:07:50 +0900

    Add Line

--- src/components/chart/HeatMap.vue
+++ src/components/chart/HeatMap.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="heatmap__container">
-    <svg id="upper_label"> </svg>
-    <svg id="left_label"> </svg>
-    <svg id="heatmap_area" class="heatmap_area" width="600" height="410">
-      <!-- <rect x='0' y='0' rx='3' ry='3' width='100' height='137' style='fill: #F8002A; opacity:0.5;' />
-      <text class='zone_group' text-anchor='middle' x='50' y='69' fill='#fff'> 123 </text> -->
-      <g v-for = "colIndex in 6" :key="colIndex" >
-        <g v-for = "rowIndex in 3" :key="rowIndex" >          
-          <rect :x='100*(colIndex -1)' :y='137*(rowIndex-1)' rx='0' ry='0' width="100" height='137' :style='getStyle(rowIndex, colIndex)' />
-          <text class='zone_group' text-anchor='middle' :x='-50 + 100*colIndex' :y='-68.5 + 137 * rowIndex' fill='#fff'> {{colIndex  + rowIndex*6 -6}} </text>
-        </g>
+    <svg id="upper_label" width="700" height="90">
+      <g v-for = "colIndex in 5" :key="colIndex" >
+        <line :x1='100+colIndex*100' y1='0' :x2='100+colIndex*100' y2='90' style='stroke: #000; stroke-width:1;' />
       </g>
+      
+      <line x1='300' y1='0' x2='300' y2='90' style='stroke: #000; stroke-width:1;' />
     </svg>
+    <div style='display: inline-flex'>
+      <svg id="left_label" width="100" height="410"> 
+        <line x1='30' y1='137' x2='100' y2='137' style='stroke: #000; stroke-width:1;' />
+        <line x1='30' y1='274' x2='100' y2='274' style='stroke: #000; stroke-width:1;' />
+      </svg>
+      <svg id="heatmap_area" class="heatmap_area" width="600" height="410">
+        <g v-for = "colIndex in 6" :key="colIndex" >
+          <g v-for = "rowIndex in 3" :key="rowIndex" >          
+            <rect :x='100*(colIndex -1)' :y='137*(rowIndex-1)' rx='0' ry='0' width="100" height='137' :style='getStyle(rowIndex, colIndex)' />
+            <text class='zone_group' text-anchor='middle' :x='-50 + 100*colIndex' :y='-68.5 + 137 * rowIndex' fill='#fff'> {{colIndex  + rowIndex*6 -6}} </text>
+          </g>
+        </g>
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -33,8 +42,6 @@ export default {
   },
   methods: {
     getStyle(row, col){
-      console.log(row, col)
-      console.log('fill: #F8002A; opacity:' + 0.06 * (row + col) + ';')
       return 'fill: #F8002A; opacity:' + 0.05 * (row + col) + ';'
     }
   },
